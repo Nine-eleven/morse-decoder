a2dbb5953c1cab40e87e7736895f1e6c2442bbf1
nine-eleven <thedevilwearspradaoops@gmail.com>
2020-10-18 14:30:36 +0300

    feat: comleted task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,64 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+
+    for (let i = 0; i+9 <= (expr.length - 1); i += 10) {
+       ((i+9) !== (expr.length - 1)) ? array.push(expr.slice (i, i+10)) : array.push(expr.slice (i));
+    }
+
+    let newArray = array.map ((item) => {
+        let resultNewArray = "";
+        for (let i = 0 ; i < (item.length - 1); i += 2) {
+            if (item[i] === "0") { continue
+            } else if (item[i] === "1" && item[i+1] === "0") { resultNewArray += "."
+            } else if (item[i] === "*") { 
+                resultNewArray = " ";
+                break;
+            } else {resultNewArray += "-"}
+        }
+
+        return resultNewArray; 
+    })
+
+    let resultString = "";
+    newArray.forEach( (item) => {
+        if (item === " ") {resultString += " "
+         } else resultString += MORSE_TABLE[item];
+} )
+
+
+   return resultString;
+
 }
 
 module.exports = {
     decode
 }
+
+
+// способ выдаёт ошибку на join!!!!
+//  let array = [];
+
+//    for (let i = 0; i+9 <= (expr.length - 1); i += 10) {
+//       ((i+9) !== (expr.length - 1)) ? array.push(expr.slice (i, i+10)) : array.push(expr.slice (i));
+//    }
+
+//    let newArray = array.map ((item) => {
+//        item.replace(/10/g, ".");
+//        item.replace(/11/g, "-");
+//        item.replace(/\*\*\*\*\*\*\*\*\*\*/g, " ");
+//        return item.match(/\D/g).join(""); 
+//    })
+
+/*    let keysArray = Object.keys(MORSE_TABLE);
+
+    let result = newArray.map ( (item) => {
+        let  valueResult = "";
+        if (keysArray.includes(item)) { valueResult = MORSE_TABLE.item};
+        return  valueResult;
+    })
+
+   return result.join(""); 
+   
+*/
